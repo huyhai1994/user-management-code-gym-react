480dec21606e79d2c622dbabb1dc9f39bc45222d
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-23 16:40:02 +0700

    validate input form ok

--- src/components/Users/User-Add/UserAdd.js
+++ src/components/Users/User-Add/UserAdd.js
@@ -20,7 +20,6 @@ function UserAdd() {
         }, validationSchema: registerSchema, onSubmit: (values) => {
             const {name, email, password} = values;
             // TODO: Dang ki nguoi dung moi
-            // navigate("/admin/users");
             console.log(values);
             alert('User registered successfully');
             registerForm.resetForm();
@@ -29,32 +28,36 @@ function UserAdd() {
     })
 
 
-    return (
-
-        <div className='container mt-5 '>
-            <h1 className='text-center'>Add new user</h1>
-            {/*TODO: Form login start here...*/}
-            <form className='border p-3 rounded-3' onSubmit={registerForm.handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                    <input type="text" name="name" onChange={registerForm.handleChange} className="form-control"
-                           id="exampleInputName"
-                           aria-describedby="nameHelp"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" name="email" onChange={registerForm.handleChange} className="form-control"
-                           id="exampleInputEmail1"
-                           placeholder="name@example.com"/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" name="password" className="form-control" id="exampleInputPassword1"
-                           onChange={registerForm.handleChange}/>
-                </div>
-                <button type="submit" className="btn btn-primary w-100">Submit</button>
-            </form>
-        </div>)
+    return (<div className='container mt-5 '>
+        <h1 className='text-center'>Add new user</h1>
+        {/*TODO: Form login start here...*/}
+        <form className='border p-3 rounded-3' onSubmit={registerForm.handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
+                <input type="text" name="name" onChange={registerForm.handleChange} className="form-control"
+                       id="exampleInputName"
+                />
+                {registerForm.errors.name && (<small className="text-danger">{registerForm.errors.name}</small>)}
+            </div>
+            {/*TODO: email input start*/}
+            <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" name="email" onChange={registerForm.handleChange} className="form-control"
+                       id="exampleInputEmail1"
+                       placeholder="name@example.com"/>
+                {registerForm.errors.email && (<small className="text-danger">{registerForm.errors.email}</small>)}
+            </div>
+            {/*TODO: password input*/}
+            <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" name="password" className="form-control" id="exampleInputPassword1"
+                       onChange={registerForm.handleChange}/>
+                {registerForm.errors.password && (
+                    <small className="text-danger">{registerForm.errors.password}</small>)}
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Submit</button>
+        </form>
+    </div>)
 }
 
 export default UserAdd;
