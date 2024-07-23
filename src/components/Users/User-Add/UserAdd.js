35270484e2575df55b27b70d9a436147c1620dde
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-23 16:31:34 +0700

    get user from submit form ok

--- src/components/Users/User-Add/UserAdd.js
+++ src/components/Users/User-Add/UserAdd.js
@@ -18,32 +18,39 @@ function UserAdd() {
         initialValues: {
             name: '', email: '', password: '',
         }, validationSchema: registerSchema, onSubmit: (values) => {
-
+            const {name, email, password} = values;
+            // TODO: Dang ki nguoi dung moi
+            // navigate("/admin/users");
+            console.log(values);
+            alert('User registered successfully');
+            registerForm.resetForm();
+            navigate("/admin/users");
         }
     })
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        /*TODO: Call API to add new user like login*/
 
-        navigate("/admin/users");
-    }
 
     return (
 
         <div className='container mt-5 '>
             <h1 className='text-center'>Add new user</h1>
-            <form className='border p-3 rounded-3'>
+            {/*TODO: Form login start here...*/}
+            <form className='border p-3 rounded-3' onSubmit={registerForm.handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                    <input type="email" className="form-control" id="exampleInputName" aria-describedby="nameHelp"/>
+                    <input type="text" name="name" onChange={registerForm.handleChange} className="form-control"
+                           id="exampleInputName"
+                           aria-describedby="nameHelp"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" name="email" onChange={registerForm.handleChange} className="form-control"
+                           id="exampleInputEmail1"
+                           placeholder="name@example.com"/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input type="password" name="password" className="form-control" id="exampleInputPassword1"
+                           onChange={registerForm.handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary w-100">Submit</button>
             </form>
