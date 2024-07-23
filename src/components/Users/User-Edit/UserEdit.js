aabee08bdcbd4ac67503f437324a8206fe682737
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-23 21:32:32 +0700

    finish function for Update

--- src/components/Users/User-Edit/UserEdit.js
+++ src/components/Users/User-Edit/UserEdit.js
@@ -6,32 +6,18 @@ import {useEffect, useState} from "react";
 
 const editSchema = Yup.object().shape({
     email: Yup.string()
-        .required("Email is required"),
-    password: Yup.string().required("Password is required"),
-    name: Yup.string().required("Name is required")
+        .required("Email is required"), name: Yup.string().required("Name is required")
 });
 const API_URL = 'https://669dd2f69a1bda3680047410.mockapi.io/users/';
 
 function UserEdit() {
     const navigate = useNavigate();
     const {id} = useParams();
-    /*TODO get the user info with id*/
-    const [loadData, setLoadData] = useState(false);
-    const [user, setUser] = useState([]);
-    useEffect(() => {
-        axios.get(`${API_URL}/${id}`)
-            .then(response => {
-                setUser(response.data);
-                console.log("User fetched successfully: ", response.data);
-            })
-            .catch(error => {
-                console.error('Error fetching user: ', error);
-            });
-    }, [loadData])
+    const [user, setUser] = useState(null);
 
     const editForm = useFormik({
         initialValues: {
-            name: '', email: '', password: '',
+            name: '', email: '',
         }, validationSchema: editSchema, onSubmit: (values) => {
             axios.put(`${API_URL}/${id}`, values).then(response => {
                 alert('User updated successfully');
@@ -41,32 +27,45 @@ function UserEdit() {
         }
     });
 
+    useEffect(() => {
+        axios.get(`${API_URL}/${id}`)
+            .then(response => {
+                setUser(response.data);
+                editForm.setValues({
+                    name: response.data.name, email: response.data.email
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching user: ', error);
+            });
+    }, [id]);
+
+    if (!user) {
+        return <div>Loading...</div>;
+    }
+
     return (<div className='container mt-5 '>
         <h1 className='text-center'>Edit user</h1>
         <form className='border p-3 rounded-3' onSubmit={editForm.handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="exampleInputName" className="form-label">Name</label>
-                <input type="text" name="name" onChange={editForm.handleChange} className="form-control"
+                <input type="text" value={editForm.values.name} name="name" onChange={editForm.handleChange}
+                       className="form-control"
                        id="exampleInputName"
                 />
                 {editForm.errors.name && (<small className="text-danger">{editForm.errors.name}</small>)}
             </div>
             <div className="mb-3">
                 <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                <input type="email" name="email" onChange={editForm.handleChange} className="form-control"
+                <input type="email" value={editForm.values.email} name="email" onChange={editForm.handleChange}
+                       className="form-control"
                        id="exampleInputEmail1"
                        placeholder="name@example.com"/>
                 {editForm.errors.email && (<small className="text-danger">{editForm.errors.email}</small>)}
             </div>
-            <div className="mb-3">
-                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                <input type="password" name="password" className="form-control" id="exampleInputPassword1"
-                       onChange={editForm.handleChange}/>
-                {editForm.errors.password && (<small className="text-danger">{editForm.errors.password}</small>)}
-            </div>
             <button type="submit" className="btn btn-primary w-100">Submit</button>
         </form>
-    </div>)
+    </div>);
 }
 
 export default UserEdit;
