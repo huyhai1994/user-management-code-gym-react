1dac85cfa8ad8dcba990cb718c125ace7e4be69a
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-26 05:17:26 +0700

    hero section ok

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import {Button, Container} from "react-bootstrap";
 import HomeImage from "../../assests/HomeImage.jpg";
+import MusicDisk from "../../assests/MusicDisk.jpg";
 import "./Home.css";
 
 
@@ -19,18 +20,36 @@ function Home() {
                     zIndex: '-1'
                 }}
             />
+            <img
+                src={MusicDisk}
+                alt="Music Disk"
+                className="position-absolute  rounded-circle"
+                style={{
+                    width: '350px',
+                    height: 'auto',
+                    left: '20px',
+                    top: '50%',
+                    transform: 'translateY(-50%)',
+                    zIndex: '1',
+                    filter: 'drop-shadow(0 0 20px #121212)'
+                }}
+            />
             <Container
                 className="position-relative text-center text-white d-flex flex-column justify-content-center h-100">
-                <h2 className="mb-4 text-top-left">Welcome to My Music Store</h2>
-                <p className="mb-4 text-top-left">
-                    Discover a wide variety of musical instruments, accessories, and more. Whether you're a beginner or
-                    a professional, we have something for everyone.
+                <p className="mb-4 header-top-left h1">Welcome to My Music Store</p>
+                <p className="mt-3 text-bg-info fw-bold h4 hero-subtitle-text">
+                    Discover a wide variety of musical instruments,<br/> accessories, and more.
                 </p>
+                <p className="h5">
+                    Whether you're a beginner or
+                    a professional,
+                </p>
+                <p className="h5"> we have something for everyone.</p>
                 <div>
-                    <Button variant="primary" as={Link} to="/login" className="me-2">
+                    <Button variant="primary" as={Link} to="/login" className="me-2" style={{width: '150px'}}>
                         Login
                     </Button>
-                    <Button className="" variant="secondary" as={Link} to="/register">
+                    <Button className="" variant="secondary" as={Link} to="/register" style={{width: "150px"}}>
                         Register
                     </Button>
                 </div>
